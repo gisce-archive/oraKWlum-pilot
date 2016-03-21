6f9a216494b190ff60b2e3bd3267376fd1792570
Xavi Torelló <info@xaviertorello.cat>
2016-03-21 12:10:18 +0100

    DataSource.test_data(collection_name)->dummy data
    Creates dummy data on collection_name

--- orakwlum/datasource/datasource.py
+++ orakwlum/datasource/datasource.py
@@ -3,6 +3,8 @@ __author__ = 'XaviTorello'
 
 import pymongo
 from datetime import datetime
+import random
+
 
 class DataSource(object):
     db = None
@@ -32,3 +34,40 @@ class Mongo(DataSource):
         self.connection = pymongo.MongoClient(self.db_connection_string)
 
         self.db = self.connection[self.db_name]
+
+
+
+
+    def test_data(self, drop = False,  collection = "test_data"):
+        cups_list = [ "ES0031300629986007HP0F", "ES0031406174543003VH0F", "ES0031405989553003MF0F", "ES0031406213600001NA0F", "ES0031300808670001QS0F"]
+
+        hour_start = 00
+        hour_end = 24
+        day_start = 15
+        day_end = 17
+        month = 03
+        year = 2016
+
+        dades_test = self.db[collection]
+
+        if drop:
+            try:
+                dades_test.drop()
+            except:
+                print "Error dropping '{}'".format(collection)
+
+        dades_mostra = []
+        for day in range(day_start, day_end):
+            for hour in range(hour_start, hour_end):
+                for cups in cups_list:
+                    dades_mostra.append({ "cups": cups, "hour": datetime(year,month,day,hour,0), "consumption_real": random.randint(0,100), "consumption_proposal":  random.randint(0,100)})
+
+
+        try:
+            resultat = dades_test.insert_many(dades_mostra)
+            print "Created {} dummy elements on '{}'".format(len(resultat.inserted_ids), collection)
+
+        except:
+            print "Error insering on '{}'".format(collection)
+
+
