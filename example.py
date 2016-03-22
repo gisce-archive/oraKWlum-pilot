aaa6fec128146822bbdc82fdf02edd643d968c0c
Xavi Torelló <info@xaviertorello.cat>
2016-03-22 11:06:07 +0100

    Datasource integrated on History & Consumption!
    * new method Mongo.get_list_unique_fields to reach all unique fields
    
    * Consumption() can handle datetime object for hour param or an array of
    int (year, month, day, hour) to
    
    * History loads history from datasource converting live JSON reached
    from Mongo to Consumption object
    
    * History.consumption represents a list of all consumptions matching the
    History definition criteria (dates range and/or cups)
    -> Pending to think about how to isolate it (days and hours, vs days)

--- example.py
+++ example.py
@@ -4,26 +4,45 @@ __author__ = 'XaviTorello'
 from orakwlum.consumption import *
 from orakwlum.datasource import *
 
-logging.basicConfig(level=logging.INFO)
 
-consum = Consumption("ES0031406229285001HS0F", 2016, 3, 2, 15)
 
-print "{} - {}: {} / {}".format(consum.cups.number, consum.hour,
-                                consum.consumption_real,
-                                consum.consumption_proposal)
+def Consumption_tester():
+    consum = Consumption("ES0031406229285001HS0F", 2016, 3, 2, 15)
 
-dades = Mongo(user="orakwlum", db="orakwlum")
+    print "{} - {}: {} / {}".format(consum.cups.number, consum.hour,
+                                    consum.consumption_real,
+                                    consum.consumption_proposal)
 
-#dades.test_data(drop=True)
 
-agg = "hour"
-sum = ["consumption_real", "consumption_proposal"]
+def Datasource_tester():
+    dades = Mongo(user="orakwlum", db="orakwlum")
 
-agregant_per_hores = dades.aggregate_sum(field_to_agg=agg,fields_to_sum=sum)
-#agregant_per_hores = dades.aggregate_count_fields(field_to_agg=agg,fields_to_count=sum)
+    #dades.test_data(drop=True)
 
-print "{} elements aggregating by '{}':".format(len(agregant_per_hores), agg)
+    agg = "hour"
+    sum = ["consumption_real", "consumption_proposal"]
 
-for entrada in agregant_per_hores:
-    for camp in entrada.iteritems():
-        print "  {}, sum: {} / {}".format(entrada['_id'], entrada['sum_consumption_real'], entrada['sum_consumption_proposal'])
+    agregant_per_hores = dades.aggregate_sum(field_to_agg=agg,fields_to_sum=sum)
+    #agregant_per_hores = dades.aggregate_count_fields(field_to_agg=agg,fields_to_count=sum)
+
+    print "{} elements aggregating by '{}':".format(len(agregant_per_hores), agg)
+
+    for entrada in agregant_per_hores:
+        for camp in entrada.iteritems():
+            print "  {}, sum: {} / {}".format(entrada['_id'], entrada['sum_consumption_real'], entrada['sum_consumption_proposal'])
+
+
+
+def History_tester():
+    date_start = datetime(2016,3,15)
+    date_end = datetime(2016,3,16)
+
+    history = History(dini=date_start, dfi=date_end)
+
+    history.dump_history()
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+History_tester()
