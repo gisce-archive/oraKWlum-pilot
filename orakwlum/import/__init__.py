f4bd51e59839512a292efed81b98657a3b27483c
Xavi Torelló <info@xaviertorello.cat>
2016-04-18 12:05:49 +0200

    Created main Import object

---
+++ orakwlum/import/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+__author__ = 'XaviTorello'
+
+from importer import Import
---
+++ orakwlum/import/importer.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+__author__ = 'XaviTorello'
+#__name__ = "Import"
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Import(object):
+    """
+    Main Import object
+    """
+
+    def __init__(self, file):
+        self.file = file
+
+    def get_type (self)
+        pass
