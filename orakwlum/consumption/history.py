10492b414099a2443dd7a799e588fe42432c5897
Xavi Torelló <info@xaviertorello.cat>
2016-04-25 13:03:23 +0200

    A Consumption now init with all the static data
    To avoid future incidents it's better to ensure that a Consumption have
    all the needed properties
    
    The History it's also modified, see the consumption_decoder method

--- orakwlum/consumption/history.py
+++ orakwlum/consumption/history.py
@@ -206,8 +206,15 @@ class History(object):
                            origin=JSON['origin'],
                            real=JSON['consumption_real'],
                            proposal=JSON['consumption_proposal'],
-                           time_disc=JSON['time_disc'])
-
+                           time_disc=JSON['time_disc'],
+                           province=JSON['province'],
+                           ZIP=JSON['ZIP'],
+                           voltage=JSON['voltage'],
+                           distributor=JSON['distributor'],
+                           origin_priority=JSON['origin_priority'],
+                           tariff=JSON['tariff'],
+                           pom_type=JSON['pom_type'],
+                           )
 
     def consumption_from_JSON(self, JSON):
         """
