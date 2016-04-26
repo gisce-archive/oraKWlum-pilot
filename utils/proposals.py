a4acdf9b5e95641741469cfd13ecff72e42feb89
Xavi Torelló <info@xaviertorello.cat>
2016-04-26 16:34:30 +0200

    Handling quick options (without end_date, #days)
    Options:
     -e END_DATE, if not provided, it creates just one proposal for start date
     -n NUMBER_OF_DAYS, number of days (time window) for each proposal

--- utils/proposals.py
+++ utils/proposals.py
@@ -7,13 +7,13 @@ from datetime import datetime, timedelta
 
 from orakwlum import Proposal
 
-
 ###################################################################################################
 # Creates the Proposals for a range of dates in multithread mode
 #
 # Call it with
 #   -s START_DATE
-#   -e END_DATE
+#   -e END_DATE, if not provided create just one proposal for start date
+#   -n NUMBER_OF_DAYS
 #   , where DATES must be informat YYYY-MM-DD; ie "2016-05-01"
 #
 # Optional parameters (overrides default config)
@@ -22,9 +22,11 @@ from orakwlum import Proposal
 #
 ###################################################################################################
 
+
 ##############################
 #           CONFIG           #
 ##############################
+# Overrided with manual parameters at run it!
 COLLECTION = "importer_test2"
 CPUS = 1
 ##############################
@@ -41,12 +43,27 @@ logging.basicConfig(
 
 parser = optparse.OptionParser()
 
-
 input_start_date = input_end_date = None
-parser.add_option('-s', '--start', dest="input_start_date", help="Start date, format %YYYY-%MM%-%DD")
-parser.add_option('-e', '--end', dest="input_end_date", help="End date, format %YYYY-%MM%-%DD")
-parser.add_option('-C', '--cpus', dest="cpu", help="Number of CPUs to user for multithread mode")
-parser.add_option('-c', '--collection', dest="collection", help="MongoDB collection where to fetch the Consumptions")
+parser.add_option('-s',
+                  '--start',
+                  dest="input_start_date",
+                  help="Start date, format %YYYY-%MM%-%DD")
+parser.add_option('-e',
+                  '--end',
+                  dest="input_end_date",
+                  help="End date, format %YYYY-%MM%-%DD")
+parser.add_option('-C',
+                  '--cpus',
+                  dest="cpu",
+                  help="Number of CPUs to user for multithread mode")
+parser.add_option('-c',
+                  '--collection',
+                  dest="collection",
+                  help="MongoDB collection where to fetch the Consumptions")
+parser.add_option('-n',
+                  '--number-days',
+                  dest="window",
+                  help="Number of days to integrate for each prediction")
 options, args = parser.parse_args()
 
 
@@ -54,19 +71,29 @@ def main():
     assert options.input_start_date, "Initial range date is not provided"
     DAY_INITIAL = datetime.strptime(options.input_start_date, "%Y-%m-%d")
 
-    assert options.input_end_date, "End range date is not provided"
-    DAY_LAST = datetime.strptime(options.input_end_date, "%Y-%m-%d")
+    try:
+        WINDOW = int(options.window)
+    except:
+        WINDOW = 1
+
+    try:
+        DAY_LAST = datetime.strptime(options.input_end_date, "%Y-%m-%d")
+    except:
+        print "End range date is not provided, creating just one proposal for {}".format(
+            DAY_INITIAL)
+        DAY_LAST = DAY_INITIAL
 
     assert DAY_INITIAL <= DAY_LAST, "End range date must be greater than initial date"
 
     CPUs = CPUS
-    if hasattr(options,"cpu") and isinstance(options.cpu,int):
-        CPUs = options.cpu
+    if hasattr(options, "cpu") and isinstance(options.cpu, int):
+        CPUs = int(options.cpu)
 
+    DAYS_LIST = datetime_list_from_range(DAY_INITIAL, DAY_LAST, WINDOW)
 
-    DAYS_LIST = datetime_list_from_range(DAY_INITIAL, DAY_LAST)
-
-    Proposal_Massive(days_list=DAYS_LIST, proposal_days_interval=1, CPUs=CPUs)
+    Proposal_Massive(days_list=DAYS_LIST,
+                     proposal_days_interval=WINDOW,
+                     CPUs=CPUs)
 
 
 def Proposal_tester(args):
@@ -78,7 +105,8 @@ def Proposal_tester(args):
 
     cups_to_filter = None
 
-    print "{}/{} Proposal init for {} - {}".format(id, max, date_start, date_end)
+    print "{}/{} Proposal init for {} - {}".format(id, max, date_start,
+                                                   date_end)
 
     proposal = Proposal(start_date=date_start,
                         end_date=date_end,
@@ -107,10 +135,11 @@ def Proposal_tester(args):
     proposal.show_proposal()
 
 
+def Proposal_Massive(days_list=None,
+                     proposal_days_interval=1,
+                     CPUs=4):
 
-def Proposal_Massive(days_list=None, proposal_days_interval=1, type="F1", CPUs=4):
-
-    assert isinstance(days_list,list), "Days list must be a list"
+    assert isinstance(days_list, list), "Days list must be a list"
 
     days_count = len(days_list)
 
@@ -119,9 +148,9 @@ def Proposal_Massive(days_list=None, proposal_days_interval=1, type="F1", CPUs=4
     # diff hardcoded functions to speedup pool.map without internal conditionals
 
     try:
-        pool.map(Proposal_tester,
-                 ((day, proposal_days_interval, idx + 1, days_count, COLLECTION)
-                  for idx, day in enumerate(days_list)))
+        pool.map(Proposal_tester, (
+            (day, proposal_days_interval, idx + 1, days_count, COLLECTION)
+            for idx, day in enumerate(days_list)))
 
     except Exception as e:
         print "Thread error at processing '{}'".format(e)
@@ -130,16 +159,14 @@ def Proposal_Massive(days_list=None, proposal_days_interval=1, type="F1", CPUs=4
     pool.join()
 
 
-
-def datetime_list_from_range( date_ini, date_end):
+def datetime_list_from_range(date_ini, date_end, proposal_window):
     days = []
     date_act = date_ini
 
     while date_act <= date_end:
         days.append(date_act)
-        date_act += timedelta(days=1)
+        date_act += timedelta(days=proposal_window)
     return days
-
 
 
 if (__name__ == "__main__"):
